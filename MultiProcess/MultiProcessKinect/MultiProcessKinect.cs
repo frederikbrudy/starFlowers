6c9bef884f3b9725648a3ce067a9c6a7bd3478f5
Janko Hofmann <info@jankohofmann.de>
2013-04-05 18:03:00 +0200

    Objektserialisierung begonnen

--- MultiProcess/MultiProcessKinect/MultiProcessKinect.cs
+++ MultiProcess/MultiProcessKinect/MultiProcessKinect.cs
@@ -7,10 +7,12 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Kinect;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace MultiProcessKinect
 {
-    class MultiProcessKinect
+    static class MultiProcessKinect
     {
         public static int testValue = 2;
         private static Mutex mutex;
@@ -31,9 +33,21 @@ namespace MultiProcessKinect
                 file = MemoryMappedFile.OpenExisting("SkeletonExchange");
                 writer = file.CreateViewAccessor();
                 int[] randomNumbers = new int[8];
+                String[] serializedSkeletons = new String[1];
+
+
                 var rndGenerator = new Random();
                 // zum Debuggen wird der Prozess mit einer Zufallszahl versehen, die er sendet
                 var processID = rndGenerator.Next(1000);
+                Skeleton skel = new Skeleton();
+
+                skel.TrackingState = SkeletonTrackingState.PositionOnly;
+                SkeletonPoint testPkt = new SkeletonPoint();
+                testPkt.X = 10;
+                testPkt.Y = 20;
+                testPkt.Z = 30;
+                skel.Position = testPkt;
+                serializedSkeletons[0] = SerializeObject<Skeleton>(skel);
 
                 while (true)
                 {
@@ -47,7 +61,10 @@ namespace MultiProcessKinect
                     try
                     {
                         mutex.WaitOne();
-                        writer.WriteArray(0, randomNumbers, 0, randomNumbers.Length);
+                        writeStringToMemoryMappedFile(SerializeBase64(skel), writer);
+                        //writer.WriteArray(0, serializedSkeletons, 0, serializedSkeletons.Length);
+                        //writer.WriteArray(0, randomNumbers, 0, randomNumbers.Length);
+                        
                         mutex.ReleaseMutex();
                         Thread.Sleep(200);
                     }
@@ -77,5 +94,54 @@ namespace MultiProcessKinect
                     mutex.ReleaseMutex();
                 }
             }*/
+
+        public static string SerializeObject<T>(this T toSerialize)
+        {
+            //try
+            //{
+                XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+                StringWriter textWriter = new StringWriter();
+
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.ToString());
+            //    return "";
+            //}
+
+        }
+
+        public static string SerializeBase64(object o)
+        {
+            // Serialize to a base 64 string
+            byte[] bytes;
+            long length = 0;
+            MemoryStream ws = new MemoryStream();
+            BinaryFormatter sf = new BinaryFormatter();
+            sf.Serialize(ws, o);
+            length = ws.Length;
+            bytes = ws.GetBuffer();
+            string encodedData = bytes.Length + ":" + Convert.ToBase64String(bytes, 0, bytes.Length, Base64FormattingOptions.None);
+            return encodedData;
+        }
+
+        public static void writeStringToMemoryMappedFile(String stringToWrite, MemoryMappedViewAccessor accessor)
+        {
+            try
+            {
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] Buffer = encoding.GetBytes(stringToWrite);
+                accessor.Write(54, (ushort)Buffer.Length);
+                accessor.WriteArray(54 + 2, Buffer, 0, Buffer.Length);
+            }
+            catch (Exception e)
+            {
+
+            }
+            
+
+        }
     } 
 }
--- MultiProcess/MultiProcessMain/MainWindow.xaml.cs
+++ MultiProcess/MultiProcessMain/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Collections;
 using System.Diagnostics;
 using System.Threading;
 using System.IO.MemoryMappedFiles;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace MultiProcessMain
 {
@@ -75,13 +77,15 @@ namespace MultiProcessMain
                             mutex = Mutex.OpenExisting("mappedfilemutex");
                             mutex.WaitOne(); // sichert alleinigen Zugriff
                             // liest Daten aus der MemoryMappedFiles
-                            accessor.ReadArray(0, someNumbers, 0, someNumbers.Length); 
-                            var output="";
+                            String xmlObject = getStringFromMemoryMappedFile(accessor);
+
+                            //accessor.ReadArray(0, someNumbers, 0, someNumbers.Length); 
+                            /*var output="";
                             foreach (int someNumber in someNumbers)
                             {
                                 output += (someNumber + " ");
-                            }
-                            Console.WriteLine(output);                           
+                            }*/
+                            Console.WriteLine(xmlObject);                           
                             mutex.ReleaseMutex(); // gibt Mutex wieder frei
                             Thread.Sleep(50); // wartet 50 ms                     
                         }
@@ -98,6 +102,28 @@ namespace MultiProcessMain
                 }
 
 
+        }
+
+        private String getStringFromMemoryMappedFile(MemoryMappedViewAccessor accessor)
+        {
+            ushort Size = accessor.ReadUInt16(54);
+            byte[] Buffer = new byte[Size];
+            accessor.ReadArray(54 + 2, Buffer, 0, Buffer.Length);
+            return ASCIIEncoding.ASCII.GetString(Buffer);
+        }
+
+        public static object DeserializeBase64(string s)
+        {
+            // We need to know the exact length of the string - Base64 can sometimes pad us by a byte or two
+            int p = s.IndexOf(':');
+            int length = Convert.ToInt32(s.Substring(0, p));
+
+            // Extract data from the base 64 string!
+            byte[] memorydata = Convert.FromBase64String(s.Substring(p + 1));
+            MemoryStream rs = new MemoryStream(memorydata, 0, length);
+            BinaryFormatter sf = new BinaryFormatter();
+            object o = sf.Deserialize(rs);
+            return o;
         }
 
         private void Window_Closing_1(object sender, System.ComponentModel.CancelEventArgs e)
